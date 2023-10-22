c28c2d9087997d5af7fee4c09d5cce803f1ee361
amichhh <amic_1111@outlook.jp>
2023-10-22 09:11:11 +0000

    create Spring Boot project

---
+++ src/test/java/music/MusicApplicationTests.java
@@ -0,0 +1,13 @@
+package music.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
