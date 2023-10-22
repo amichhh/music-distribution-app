1f3629151647e42dd93ce7a1046795506e889714
amichhh <amic_1111@outlook.jp>
2023-10-22 09:12:58 +0000

    create Spring Boot project

--- src/main/java/music/MusicApplication.java
+++ src/main/java/music/MusicApplication.java
@@ -1,4 +1,4 @@
-package music.music;
+package music;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MusicApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MusicApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MusicApplication.class, args);
+    }
 
 }
